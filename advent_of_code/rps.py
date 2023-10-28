2ee5b4afee59ce01e811684ca4f6d141152f3db0
bagons <104223895+bagons@users.noreply.github.com>
2023-10-28 08:58:53 +0200

    Update rps.py

--- advent_of_code/rps.py
+++ advent_of_code/rps.py
@@ -15,5 +15,5 @@ inpt = """A Y
 B X
 C Z"""
 
-# rozdělí vtup na řádky, najde volby předmětů -> zjistí skóre kola -> udělá součet všech skóre z ruzných kol
+# rozdělí vstup na řádky, najde volby předmětů -> zjistí skóre kola -> udělá součet všech skóre z ruzných kol
 print(reduce(lambda b, c: b + c, list(map(lambda a: scor_check(a[0], a[2]), inpt.split("\n")))))

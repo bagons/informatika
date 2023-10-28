6b0268708344973e4e3832f4068b9f7899119db5
bagons <104223895+bagons@users.noreply.github.com>
2023-10-28 09:02:39 +0200

    Update rucksacks.py

--- advent_of_code/rucksacks.py
+++ advent_of_code/rucksacks.py
@@ -10,8 +10,11 @@ def char_to_score(char):
 
 def solve():
     score = 0
+    # projde každý rucksack
     for rucksack in inpt.split("\n"):
+        # 2. kapsa rucksacku
         compartment_2 = rucksack[int(len(rucksack) / 2):]
+        # hledá jestli je v 1. kapse stejný item jako v 2.
         for char in rucksack[0: int(len(rucksack) / 2)]:
             if char in compartment_2:
                 print(char)
